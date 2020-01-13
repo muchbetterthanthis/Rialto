4967d1a715c6a801f33317a4de28439f0d858854
gioianm <batchatmzf@gmail.com>
2020-01-13 16:20:06 +0200

    Branch creation

---
+++ Rialto.EF/Repositories/LotRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rialto.Data.Repositories;
+
+namespace Rialto.EF.Repositories
+{
+    class LotRepository:ILotRepository
+    {
+        void Crea
+    }
+}
