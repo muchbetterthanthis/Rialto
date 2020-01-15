0bf19789c83a6603ad129f0f0a6278a46065d849
Alex <33702786+muchbetterthanthis@users.noreply.github.com>
2020-01-15 23:23:17 +0200

    Add files via upload
    interfaces ILot and ILotDynamics added

---
+++ Rialto.Data/Interfaces/ILot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rialto.Data.Interfaces
+{
+    public interface ILot
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        double Price { get; set; }
+        List<ILotDynamic> Dynamics { get; set; }
+
+        void changePrice(double newPrice);
+
+        double getCurrentPrice();
+
+        double getPriceAtDate(DateTime atDate);
+    }
+}
---
+++ Rialto.Data/Interfaces/ILotDynamic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rialto.Data.Interfaces
+{
+    public interface ILotDynamic
+    {
+        int ID { get; set; }
+        DateTime DateOfChange { get; set; }
+        double Delta { get; set; }
+    }
+}
